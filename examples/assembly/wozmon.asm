32b11fa9c714bc79fb3592236d64cdbafeb77c48
Joshua Farmer <rhubarbjosh@outlook.com>
2024-11-23 22:03:20 +0000

    x

--- examples/assembly/wozmon.asm
+++ examples/assembly/wozmon.asm
@@ -4,29 +4,41 @@
 	define variables 0
 	; FIRST PROGRAM TO USE NEW STACK POINTERS!! (s4 to z7)
 	; yes, this cpu has 16 stack pointers/address registers...
+	; PROGRAMMERS NOTE :
+	; DONT USE S0 FOR ADDRESSES,
+	; IT IS OVEWRITTEN ALOT WITHOUT SAVING
+	; IN THIS CODE
 _start:
 	push .#2, s3
 	pop p1, s3
 	ld s3, %stack
-	ld s0, %data
+	ld s1, %data
 	call %_gets
 _inf:
 	jmp %_inf
 
+	; s1 is the input address
+	; x6 is the length
 _gets:
-	ld w0, #3
-	xc w0, s0
+	ld s0, #3
 	ld x7, #2
-_l0:
+	ld x6, #0
+_f0:
 	in
 	cmp #0
-	bz %_l0
-	cmp #13
-	retz
+	bz %_f0
 	cmp #10
-	retz
-_put:
+	bz %_f2
+_f1:
 	xc x7, s0
 	out
 	xc x7, s0
+	poke a, s1
+	inc s1
+	inc x6
 	jmp %_l0
+_f2:
+	ld x5, s1
+	sub x5, x6
+	ld s1, x5
+	ret
