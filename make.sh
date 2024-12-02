2c0f7c536fcd88b35c6b2cc913c937b4b00e7af7
Joshua Farmer <rhubarbjosh@outlook.com>
2024-12-02 17:05:10 +0000

    a

--- make.sh
+++ make.sh
@@ -1,3 +1,2 @@
 g++ "src/C++/main.cpp" -o "bin/C++/vop" -Wall -Wextra -Werror -Wpedantic -lSDL2 -O3
 gcc "src/C/main.c" -o "bin/C/vop" -Wall -Wextra -Werror -Wpedantic -lSDL2 -O3 -std=c99
-rustc "src/Rust/main.rs" -o "bin/Rust/vop"
